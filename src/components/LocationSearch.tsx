a8436e8f644e413aa6237adafe8e0896d27acfab
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-03-28 11:07:47 +0545

    handle form submission in ts

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -7,5 +7,29 @@ interface LocationSearchProps {
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const [places, setPlaces] = useState<Place | null>([]);
-  return <div>Location Search</div>;
+  const [term, setTerm] = useState('');
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event?.preventDefault();
+    console.log('Need to search API..', term);
+  }
+
+  return (
+    <div>
+      <form action='' onSubmit={handleSubmit}>
+        <label htmlFor='term' className='font-bold'>
+          Search
+        </label>
+
+        <input
+          type='text'
+          className='borser border-gray-300 rounded-md shadow-md focusLborder:indigo'
+          id='term'
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
