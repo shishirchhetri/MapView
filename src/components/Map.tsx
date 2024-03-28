54b72ce1b470debc893075d07dc76cb8982dc841
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-03-28 10:36:55 +0545

    implement type on props

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,9 @@
 import type { Place } from '../api/place';
 
-export default function Map() {
+interface MapProps {
+  place: Place | null;
+}
+
+export default function Map({ place }: MapProps) {
   return <div>Map</div>;
 }
